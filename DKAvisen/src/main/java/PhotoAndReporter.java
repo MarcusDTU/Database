ebe9e6bfd5dbc3c2ca5223c56760fa99e861d7bf
Nikolaj Schæbel <s220471@dtu.dk>
2024-03-22 13:46:44 +0100

    Fixed issue in PhotoAndReporter

--- DKAvisen/src/main/java/PhotoAndReporter.java
+++ DKAvisen/src/main/java/PhotoAndReporter.java
@@ -4,9 +4,9 @@ public class PhotoAndReporter {
 	private final Reporter reporter;
 	private final Photo photo;
 
-	public PhotoAndReporter(String title, Date date, Integer cpr, String firstName, String middleName, String lastName, String streetName, Integer civicNumber, Integer zipCode, String workPhoneNum, String privatePhoneNum, String Email) {
-		reporter = new Reporter(cpr, firstName, lastName, streetName, civicNumber, zipCode, );
-		photo = new Photo(title, date);
+	public PhotoAndReporter(String title, Date date, String cprNo, String firstName, String middleName, String lastName, String streetName, Integer civicNumber, Integer zipCode, String workPhoneNum, String privatePhoneNum, String Email) {
+		reporter = new Reporter(cpr, firstName, middleName, lastName, streetName, civicNumber, zipCode, workPhoneNum, privatePhoneNum, Email);
+		photo = new Photo(title, date, cprNo);
 	}
 
 	public Reporter getReporter() {
