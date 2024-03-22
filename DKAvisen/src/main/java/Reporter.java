fcf994593c463761403cdc2f18ce24b9ba681154
Daniel Lerche <daniel.lerche10@gmail.com>
2024-03-22 13:50:09 +0100

    Added middle name to reporter

--- DKAvisen/src/main/java/Reporter.java
+++ DKAvisen/src/main/java/Reporter.java
@@ -2,20 +2,21 @@ public class Reporter {
 
     private final String cpr;
     private final String firstName;
+    private final String middleName;
     private final String lastName;
     private final String streetName;
     private final Integer civicNumber;
     private final Integer zipCode;
     private final String country;
 
-    public Reporter(String cpr, String firstName, String lastName, String streetName, Integer civicNumber, Integer zipCode, String country) {
+    public Reporter(String cpr, String firstName, String middleName, String lastName, String streetName, Integer civicNumber, Integer zipCode) {
         this.cpr = cpr;
         this.firstName = firstName;
+        this.middleName = middleName;
         this.lastName = lastName;
         this.streetName = streetName;
         this.civicNumber = civicNumber;
         this.zipCode = zipCode;
-        this.country = country;
     }
 
     public String getCPR() { return cpr; }
@@ -24,6 +25,10 @@ public class Reporter {
         return firstName;
     }
 
+    public String getMiddleName() {
+        return middleName;
+    }
+ 
     public String getLastName() {
         return lastName;
     }
