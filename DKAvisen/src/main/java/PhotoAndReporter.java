37a8d8e2d3a8ee3b3b2b0132ea90bb27fd03ecc2
Nikolaj Schæbel <s220471@dtu.dk>
2024-03-22 13:48:55 +0100

    Expanded PhotoAndReporter

--- DKAvisen/src/main/java/PhotoAndReporter.java
+++ DKAvisen/src/main/java/PhotoAndReporter.java
@@ -4,9 +4,9 @@ public class PhotoAndReporter {
 	private final Reporter reporter;
 	private final Photo photo;
 
-	public PhotoAndReporter(String title, Date date, String cprNo, String firstName, String middleName, String lastName, String streetName, Integer civicNumber, Integer zipCode, String workPhoneNum, String privatePhoneNum, String Email) {
+	public PhotoAndReporter(String title, Date date, String cpr, String firstName, String middleName, String lastName, String streetName, Integer civicNumber, Integer zipCode, String workPhoneNum, String privatePhoneNum, String Email) {
 		reporter = new Reporter(cpr, firstName, middleName, lastName, streetName, civicNumber, zipCode, workPhoneNum, privatePhoneNum, Email);
-		photo = new Photo(title, date, cprNo);
+		photo = new Photo(title, date, cpr);
 	}
 
 	public Reporter getReporter() {
